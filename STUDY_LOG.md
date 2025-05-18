8ad63dc64e26060714a2561a3df0282daf23cd94
chen weichen <newsforcwc@gmail.com>
2025-05-18 17:27:12 +0800

    [feat] vpc and s3 on aws lab

--- STUDY_LOG.md
+++ STUDY_LOG.md
@@ -1,6 +1,6 @@
-###5/15
+### 5/15
 
-#####s3
+##### s3
 
 1. s3
 2. s3 policy
@@ -10,3 +10,20 @@
    - BucketOwnerPreferred: Bucket Owner owns all objects if no acl is used
    - BucketOwnerEnforced: ACL is always ignored, bucket owner always owns everything
    - ObjectWriter: Object writer owns everything
+
+Questions:
+
+1. when to use acl and when not to? is it for log services?
+   - for cloudfront write -> in which step does the frontend directly upload stuff to s3?
+   - could use OAC instead
+2. presigned_url not used in app??
+3. how does django native FileField and ImageField deal with it?
+
+### 5/18
+
+1. setup a VPC with one public subnet and one private subnet
+2. setup s3
+
+##### RDS
+
+##### ElastiCache
--- main.tf
+++ main.tf
@@ -30,10 +30,14 @@ resource "aws_internet_gateway" "main" {
   }
 }
 
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 resource "aws_subnet" "public" {
-  vpc_id                  = aws_vpc.main.vpc_id
+  vpc_id                  = aws_vpc.main.id
   map_public_ip_on_launch = true
-  availability_zone       = "ap-northeast-1"
+  availability_zone       = data.aws_availability_zones.available.names[0]
   cidr_block              = "10.0.1.0/24"
 
 
@@ -43,8 +47,8 @@ resource "aws_subnet" "public" {
 }
 
 resource "aws_subnet" "private" {
-  vpc_id            = aws_vpc.main.vpc_id
-  availability_zone = "ap-northeast-1"
+  vpc_id            = aws_vpc.main.id
+  availability_zone = data.aws_availability_zones.available.names[0]
   cidr_block        = "10.0.2.0/24"
 
   tags = {
@@ -52,174 +56,174 @@ resource "aws_subnet" "private" {
   }
 }
 
-resource "aws_eip" "nat" {
-  vpc = true
-}
+# resource "aws_eip" "nat" {
+#   vpc = true
+# }
 
-resource "aws_nat_gateway" "main" {
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public.id
-
-
-  tags = {
-    Name = "main"
-  }
-}
+# resource "aws_nat_gateway" "main" {
+#   allocation_id = aws_eip.nat.id
+#   subnet_id     = aws_subnet.public.id
 
 
-#public route table
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.vpc_id
-
-  route = {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
-  }
-
-  tags = {
-    Name = "main"
-  }
-}
-
-resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public.id
-}
-
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.main.id
-
-  route = {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.main.id
-  }
-
-  tags = {
-    Name = "main"
-  }
-}
-
-resource "aws_route_table_association" "private" {
-  subnet_id      = aws_subnet.private.id
-  route_table_id = aws_route_table.private.id
-}
+#   tags = {
+#     Name = "main"
+#   }
+# }
 
 
-resource "aws_ecr_repository" "main" {
-  name = "main"
-}
+# #public route table
+# resource "aws_route_table" "public" {
+#   vpc_id = aws_vpc.main.vpc_id
+
+#   route = {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.main.id
+#   }
+
+#   tags = {
+#     Name = "main"
+#   }
+# }
+
+# resource "aws_route_table_association" "public" {
+#   subnet_id      = aws_subnet.public.id
+#   route_table_id = aws_route_table.public.id
+# }
+
+# resource "aws_route_table" "private" {
+#   vpc_id = aws_vpc.main.id
+
+#   route = {
+#     cidr_block     = "0.0.0.0/0"
+#     nat_gateway_id = aws_nat_gateway.main.id
+#   }
+
+#   tags = {
+#     Name = "main"
+#   }
+# }
+
+# resource "aws_route_table_association" "private" {
+#   subnet_id      = aws_subnet.private.id
+#   route_table_id = aws_route_table.private.id
+# }
 
 
-resource "aws_ecs_cluster" "backend" {
-  name = "backend-cluster"
-}
+# resource "aws_ecr_repository" "main" {
+#   name = "main"
+# }
 
 
-# setup a role
-resource "aws_iam_role" "ecs_task_execution" {
-  name               = "ecsTaskExecutionRole"
-  assume_role_policy = data.aws_iam_policy_document.ecs_assume_role.json
-}
-
-data "aws_iam_policy_document" "ecs_assume_role" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_execution_attach" {
-  role       = aws_iam_role.ecs_task_execution
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy" # predefined by aws
-}
-
-resource "aws_cloudwatch_log_group" "backend" {
-  name = "/ecs/backend"
-}
-
-resource "aws_ecs_task_definition" "backend" {
-  family                   = "backend"
-  requires_compatibilities = ["FARGATE"]
-  network_mode             = "awsvpc"
-  cpu                      = "2"
-  memory                   = "512"
-
-  execution_role_arn = aws_iam_role.ecs_task_execution.arn
-  task_role_arn      = aws_iam_role.ecs_task_execution.arn
-
-  container_definition = jsonencode([
-    {
-      name      = "backend"
-      image     = "...." # from ecr
-      essential = true
-      portMappings = [
-        {
-          containerPort = 8000
-          hostPort      = 8000
-          protocol      = "tcp"
-        }
-      ]
-      logConfiguration = {
-        logDriver = "awslogs"
-        options = {
-          awslogs-group         = aws_cloudwatch_log_group.backend
-          awslogs-create-group  = true
-          awslogs-region        = "ap-northeast-1"
-          awslogs-stream-prefix = "ecs"
-        }
-      }
-      environment = [
-        {
-          name  = "ENV"
-          value = "production"
-        }
-      ]
-    }
-  ])
-}
-
-resource "aws_ecs_service" "backend" {
-  name                   = "backend"
-  cluster                = aws_ecs_cluster.backend.id
-  task_definition        = aws_ecs_task_definition.backend.arn
-  desired_count          = 2
-  launch_type            = "FARGATE"
-  enable_execute_command = true
-
-  network_configuration {
-    subnets          = [aws_subnet.private.id]
-    security_groups  = [aws_security_group.ecs_sg.id]
-    assign_public_ip = false
-  }
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.app_tg.arn
-    container_name   = "backend"
-    container_port   = 8000
-  }
-
-  depends_on = [aws_lb_listener.app_listener]
-}
+# resource "aws_ecs_cluster" "backend" {
+#   name = "backend-cluster"
+# }
 
 
+# # setup a role
+# resource "aws_iam_role" "ecs_task_execution" {
+#   name               = "ecsTaskExecutionRole"
+#   assume_role_policy = data.aws_iam_policy_document.ecs_assume_role.json
+# }
 
-#alb, security_group
+# data "aws_iam_policy_document" "ecs_assume_role" {
+#   statement {
+#     actions = ["sts:AssumeRole"]
+#     principals {
+#       type        = "Service"
+#       identifiers = ["ecs-tasks.amazonaws.com"]
+#     }
+#   }
+# }
+
+# resource "aws_iam_role_policy_attachment" "ecs_execution_attach" {
+#   role       = aws_iam_role.ecs_task_execution
+#   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy" # predefined by aws
+# }
+
+# resource "aws_cloudwatch_log_group" "backend" {
+#   name = "/ecs/backend"
+# }
+
+# resource "aws_ecs_task_definition" "backend" {
+#   family                   = "backend"
+#   requires_compatibilities = ["FARGATE"]
+#   network_mode             = "awsvpc"
+#   cpu                      = "2"
+#   memory                   = "512"
+
+#   execution_role_arn = aws_iam_role.ecs_task_execution.arn
+#   task_role_arn      = aws_iam_role.ecs_task_execution.arn
+
+#   container_definition = jsonencode([
+#     {
+#       name      = "backend"
+#       image     = "...." # from ecr
+#       essential = true
+#       portMappings = [
+#         {
+#           containerPort = 8000
+#           hostPort      = 8000
+#           protocol      = "tcp"
+#         }
+#       ]
+#       logConfiguration = {
+#         logDriver = "awslogs"
+#         options = {
+#           awslogs-group         = aws_cloudwatch_log_group.backend
+#           awslogs-create-group  = true
+#           awslogs-region        = "ap-northeast-1"
+#           awslogs-stream-prefix = "ecs"
+#         }
+#       }
+#       environment = [
+#         {
+#           name  = "ENV"
+#           value = "production"
+#         }
+#       ]
+#     }
+#   ])
+# }
+
+# resource "aws_ecs_service" "backend" {
+#   name                   = "backend"
+#   cluster                = aws_ecs_cluster.backend.id
+#   task_definition        = aws_ecs_task_definition.backend.arn
+#   desired_count          = 2
+#   launch_type            = "FARGATE"
+#   enable_execute_command = true
+
+#   network_configuration {
+#     subnets          = [aws_subnet.private.id]
+#     security_groups  = [aws_security_group.ecs_sg.id]
+#     assign_public_ip = false
+#   }
+
+#   load_balancer {
+#     target_group_arn = aws_lb_target_group.app_tg.arn
+#     container_name   = "backend"
+#     container_port   = 8000
+#   }
+
+#   depends_on = [aws_lb_listener.app_listener]
+# }
 
 
 
-# s3
+# #alb, security_group
+
+
+
+# # s3
 resource "aws_s3_bucket" "filestore" {
-  bucket = "filestore"
-  tags   = "filestore"
+  bucket = "will-filestore"
+  force_destroy = true
 }
 
 
 resource "aws_s3_bucket_policy" "filestore" {
-  bucket = aws_s3_bucket.filestore
-  policy = aws_s3_iam_policy_document.s3_filestore.json
+  bucket = aws_s3_bucket.filestore.id
+  policy = data.aws_iam_policy_document.s3_filestore.json
 }
 
 resource "aws_s3_bucket_ownership_controls" "filestore" {
@@ -231,20 +235,22 @@ resource "aws_s3_bucket_ownership_controls" "filestore" {
 }
 
 
-data "aws_s3_iam_policy_document" "s3_filestore" {
+data "aws_iam_policy_document" "s3_filestore" {
   statement {
     actions   = ["s3:getObject"]
+    effect = "Allow"
+
     resources = ["${aws_s3_bucket.filestore.arn}/*"]
 
-    principals {
-      type        = "AWS"
-      identifiers = ["arn:aws:iam::123456789012:role/my-ecs-task-role"] #example for now
-    }
+    # principals {
+    #   type        = "AWS"
+    #   identifiers = ["*"]
+    # }
   }
 }
 
 
-resource "aws_s3_public_access_block" "filestore" {
+resource "aws_s3_bucket_public_access_block" "filestore" {
   bucket                  = aws_s3_bucket.filestore.id
   block_public_acls       = true
   block_public_policy     = true
